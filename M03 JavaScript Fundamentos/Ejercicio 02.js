eeab3feab77935e1c9b909ebcda5c1f25209875b
florencianieto <florencia.nieto@icloud.com>
2023-09-27 13:48:52 -0300

    Ejercicios resueltos - MO3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -36,7 +36,7 @@ function multiplica(x, y) {
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
-   return (x % y);
+   return (x%y);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
